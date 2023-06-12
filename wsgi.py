4edca953aae2cc3cad5ced2fa5b9bbb8bd229a77
Tanay-Karekar <tanaykarekar15@gmail.com>
2023-06-12 09:53:08 +0530

    i hope it works this time

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,10 @@
-import sys
-sys.path.insert(0, '')
+# import sys
+# sys.path.insert(0, '')
+# from app import app
+
+# if __name__ == '__main__':
+#     app.run()
+
 from app import app
 
 if __name__ == '__main__':
